21fe36f61e264227dab4959184cb57402e5e9f22
Maurice Tadros <maurice@bowriverstudio.com>
2022-02-10 17:32:01 -0700

    Fixed Query Issue

--- src/components/TestOnQuery.tsx
+++ src/components/TestOnQuery.tsx
@@ -7,12 +7,15 @@ function TestOnQuery(): JSX.Element {
 
   const postOrPage = useQuery().nodeByUri({ uri: "/about" });
   // 1) When I uncomment the "Post" I get a runtime error `TypeError: can't access property "title", _Post is undefined`
-  const {
-    $on: {
-      //   Post: { title: postTitle, content: postContent },
-      Page: { title: pageTitle, content: pageContent },
-    },
-  } = postOrPage;
+
+  const postTitle = postOrPage?.$on?.Post?.title();
+  const postContent = postOrPage?.$on?.Post?.content();
+
+  const pageTitle = postOrPage?.$on?.Page?.title();
+  const pageContent = postOrPage?.$on?.Page?.content();
+
+  const title = postTitle || pageTitle;
+  const content = postContent || pageContent;
 
   // const title = postTitle || pageTitle;
   // const content = postContent || pageContent;
@@ -21,7 +24,7 @@ function TestOnQuery(): JSX.Element {
     <section>
       Test On Query
       <pre>{JSON.stringify(postOrPage.id, null, 4)}</pre>
-      <pre>pageTitle: {JSON.stringify(pageTitle(), null, 4)}</pre>
+      <pre>pageTitle: {JSON.stringify(title, null, 4)}</pre>
       <pre>usePage() id: {JSON.stringify(page?.id, null, 4)}</pre>
       <pre>usePage() title: {JSON.stringify(page?.title(), null, 4)}</pre>
       {/* {/* <pre>{JSON.stringify(title(), null, 4)}</pre> */}
